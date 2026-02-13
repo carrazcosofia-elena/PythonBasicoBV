66cba4af5517c1d387619afbf4227beb6267214b
carrazcosofia-elena <sofiacarracop@gmail.com>
2026-02-12 18:41:06 -0800

    Guardo mi sesion 02 de pythonbasico

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_tuplas =  (2,4)
+print("Mi tupla", mi_tuplas)
+
+#lista
+mi_lista = [1, 3.1416, "Sofia", mi_tuplas]
+print("El primer elemento de mi lista", mi_lista[0])
+print("El cuarto elemento de mi lista", mi_lista[3])
+print("El tercer elemento de mi lista", mi_lista[2])
