9c55889b4edee6827acb8988b5b221e9c4e839f7
carrazcosofia-elena <sofiacarrazcop@gmail.com>
2026-02-13 15:32:33 -0800

    Guardado modificacion en sesion 03

--- sesion_03.py
+++ sesion_03.py
@@ -17,3 +17,18 @@ mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 for i in mi_lista_2:
         if i != "lunes":
                 print(f"Feliz{i}!")
+                
+
+
+#while loop
+i = 0
+
+while i < 5:
+        i += 1
+        if i==3:
+           continue
+        print(i)
+        if i == 4:
+         break
+else:
+        print("i es ahora mayor que 5")
