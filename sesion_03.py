47e8cd8dd00ff8ec5b21793d3765825f57c6875c
carrazcosofia-elena <sofiacarrazcop@gmail.com>
2026-02-13 16:00:41 -0800

    Guardando los datos idde dias en sesion 03

--- sesion_03.py
+++ sesion_03.py
@@ -32,3 +32,28 @@ while i < 5:
          break
 else:
         print("i es ahora mayor que 5")
+
+#Practica 2
+#Dada la lista mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+#imprimir caa elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+#pista: usa los dos tipo loops while y for en el mismo progrma para lograrlo
+#resultado:
+#martes
+#miercoles
+#jueves
+#viernes
+#martes
+#miercoles
+#jueves
+#viernes
+#martes
+#miercoles
+#jueves
+#viernes
+i = 0
+mi_lista_2 = ["lunes", "martes", "miercoles", "miercoles", "jueves", "viernes"]
+while i < 3:
+       i += 1
+       for d in mi_lista_2:
+         if d != "lunes":
+           print(f"{d}!")
